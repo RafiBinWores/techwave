15eba527b7b0e5dc6796572ca2e6458a069df089
Rafi Bin Wores <rafibinwores2203@gmail.com>
2026-08-15 15:49:04 +0600

    [ADD] Expires pdf consol command

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('compressed-images:cleanup')->dailyAt('03:00');
 Schedule::command('bg-removed-images:cleanup')->dailyAt('03:10');
 Schedule::command('resized-images:cleanup')->dailyAt('03:20');
+Schedule::command('pdf-files:cleanup')->dailyAt('03:30');
